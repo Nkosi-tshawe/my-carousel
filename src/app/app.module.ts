6bfcec6f0ffb74cd71970d4b5247ce59fda214db
Nkosi Tshawe <Nkosi.Tshawe@westerncape.gov.za>
2021-05-21 07:20:45 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardComponent } from './components/card/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
